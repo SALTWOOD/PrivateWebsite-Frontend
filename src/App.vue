a11588a81ea4f02aefde7ccfb2d165365f0fbe78
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-11-18 00:15:26 +0800

    Release 1.0.3

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,36 @@
 </template>
 
 <script lang="ts" setup>
+import config from "../package.json";
+
+const about = String.raw`
+This project is created by SaltWood for his own use.
+Any non-commercial use is welcome, although this project was not created for others.
+Current version: ${config.version}.
+
+PrivateWebsite is a personal website
+
+This software follows the MIT license.
+Copyright (c) 2024 SaltWood.
+
+https://github.com/SALTWOOD/PrivateWebsite-Frontend
+https://github.com/SALTWOOD/PrivateWebsite-Backend
+`;
+// @ts-ignore
+window.about = () => console.log(about);
+
+const tip = String.raw`
+/*
+ *      ____            _       __     __         _ __     
+ *     / __ \          | |     / /__  / /_  _____(_) /____ 
+ *    / /_/ /  ______  | | /| / / _ \/ __ \/ ___/ / __/ _ \
+ *   / ____/  /_____/  | |/ |/ /  __/ /_/ (__  ) / /_/  __/
+ *  /_/                |__/|__/\___/_.___/____/_/\__/\___/ 
+ *                       v${config.version} | Created by SaltWood
+ *                       Type "about()" for more information.
+ */
+`;
+console.log(tip);
+console.log("你怎么那么无聊来看浏览器的控制台？这里可啥都没有哦。");
+console.log("不过……这句话咋那么熟悉呢……？");
 </script>
