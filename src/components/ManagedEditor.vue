1dd5f00fed91b2e9a9f81a6a725b0e03312f9d48
SALTWOOD <105980161+SALTWOOD@users.noreply.github.com>
2024-11-28 23:12:51 +0800

    feat: 拖拽上传文件

--- src/components/ManagedEditor.vue
+++ src/components/ManagedEditor.vue
@@ -1,13 +1,15 @@
 <template>
-    <MdEditor v-model="props.content" :preview="vuetify.display.mdAndUp.value"
-        :theme="(vuetify.theme.global.name.value as 'dark' | 'light')" @on-upload-img="onUploadImg" />
+    <MdEditor ref="editor" v-model="props.content" :preview="vuetify.display.mdAndUp.value"
+        :theme="(vuetify.theme.global.name.value as 'dark' | 'light')" @on-upload-img="onUploadImg" @on-drop="onDrop" />
 </template>
 
 <script setup lang="ts">
-import { MdEditor } from 'md-editor-v3';
+import { ExposeParam, MdEditor } from 'md-editor-v3';
 import vuetify from '@/plugins/vuetify';
 import 'md-editor-v3/lib/style.css';
 import { FileUploader } from '@/utils/FileUploader';
+
+const editor = ref<ExposeParam>();
 
 const props = defineProps({
     content: {
@@ -17,13 +19,55 @@ const props = defineProps({
 });
 
 async function onUploadImg(files: File[], callback: (url: string[] | { url: string, alt: string, title: string }[]) => void): Promise<void> {
-    const uploader = new FileUploader();
     const urls = [];
     for (const file of files) {
+        const uploader = new FileUploader();
         await uploader.startUploadSession(file);
         await uploader.uploadFile(file);
-        urls.push(uploader.fileUrl);
+        if (uploader.uploadSuccess) {
+            urls.push({
+                url: uploader.fileUrl,
+                alt: file.name,
+                title: file.name
+        });
+        } else {
+            console.error('Upload failed');
+            console.error(uploader.uploadError);
+            urls.push({
+                url: 'Upload failed',
+                alt: file.name,
+                title: file.name
+            });
+        }
     }
     callback(urls);
+}
+
+async function onDrop(e: DragEvent): Promise<void> {
+    if (e.dataTransfer?.files) {
+        const files = Array.from(e.dataTransfer.files);
+        for (const file of files) {
+            const uploader = new FileUploader();
+            await uploader.startUploadSession(file);
+            await uploader.uploadFile(file);
+            if (uploader.uploadSuccess) {
+                editor.value?.insert((selectedText: string) => (
+                    {
+                        targetValue: `[${file.name}](${uploader.fileUrl})\n`,
+                        select: false
+                    }
+                ));
+            } else {
+                console.error('Upload failed');
+                console.error(uploader.uploadError);
+                editor.value?.insert((selectedText: string) => (
+                    {
+                        targetValue: `[${file.name}](Upload failed)\n`,
+                        select: false
+                    }
+                ));
+            }
+        }
+    }
 }
 </script>
